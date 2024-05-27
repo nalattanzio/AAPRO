edc8d54808b5a181d5c197edc1aff5db0d44a64e
nalattanzio <nalattanzio@gmail.com>
2024-05-27 00:48:30 -0300

    de todo un poco

---
+++ funciones.js
@@ -0,0 +1,18 @@
+alert("Qué hacé guacho??");
+
+prompt("Poné algo guacho!:");
+
+console.log("");
+console.warm("");
+console.error("");
+console.info("");
+
+function buscarEnGoogle(){
+    var cadena = document.getElementById(titulo);
+    for (let index = 0; index < cadena.length; index++) {
+        const element = array[index];
+        
+    }
+}
+
+<a href="https://www.google.com/search?q=ChatGPT" target="_blank">Buscar ChatGPT en Google</a>
