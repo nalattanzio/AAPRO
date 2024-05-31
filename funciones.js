6c065adfd748a723c4ec3cefa7ce196f72c3b2e0
nalattanzio <nalattanzio@gmail.com>
2024-05-31 01:20:29 -0300

    Cambios en Generos

--- funciones.js
+++ funciones.js
@@ -1,7 +1,75 @@
-alert("Qué hacé guacho??");
+document.getElementById("formGeneros").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
+    var selectElement = document.getElementById("generos"); //Guarda todas las opciones del SELECT "generos"
+    var selectedValue = selectElement.value; // Obtiene el valor de la opción seleccionada
 
-prompt("Poné algo guacho!:");
+    var elegido = '';
+    switch(selectedValue){
+        case 'value1':
+            elegido = 'ACCION';
+            break;
+        case 'value2':
+            elegido = 'AVENTURA';
+            break;
+        case 'value3':
+            elegido = 'COMEDIA';
+            break;
+        case 'value4':
+            elegido = 'DRAMA';
+            break;
+        case 'value5':
+            elegido = 'CIENCIA FICCION';
+            break;
+        case 'value6':
+            elegido = 'FANTASIA';
+            break;
+        case 'value7':
+            elegido = 'TERROR';
+            break;
+        case 'value8':
+            elegido = 'MISTERIO';
+            break;
+        case 'value9':
+            elegido = 'SUSPENSO';
+            break;
+        case 'value10':
+            elegido = 'ROMANCE';
+            break;
+        case 'value11':
+            elegido = 'ANIMACION';
+            break;
+        case 'value12':
+            elegido = 'DOCUMENTAL';
+            break;
+        case 'value13':
+            elegido = 'CRIMEN';
+            break;
+        case 'value14':
+            elegido = 'WESTERN';
+            break;
+        case 'value15':
+            elegido = 'MUSICAL';
+            break;
+        case 'value16':
+            elegido = 'GUERRA';
+            break;
+        case 'value17':
+            elegido = 'BIOGRAFICO';
+            break;
+        case 'value18':
+            elegido = 'HISTORICO';
+            break;
+        case 'value19':
+            elegido = 'DEPORTES';
+    }
 
+    const elemento = document.getElementById('generote');
+    elemento.textContent = elegido;
+    
+  });
+
+
+  /*
 console.log("");
 console.warm("");
 console.error("");
@@ -16,3 +84,4 @@ function buscarEnGoogle(){
 }
 
 <a href="https://www.google.com/search?q=ChatGPT" target="_blank">Buscar ChatGPT en Google</a>
+*/
